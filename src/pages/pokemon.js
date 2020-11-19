1e1b6ff9d181616af8941f83bfd4048796eb9663
Mohammed Amjed <35508166+MohammedAmj@users.noreply.github.com>
2020-11-19 21:18:12 +0200

    link fix

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Link from "next/lclsink";
+import Link from "next/link";
 
 export default function pokemon({ poke }) {
   return (
